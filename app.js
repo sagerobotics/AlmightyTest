b033cfa63d8b7136e20b6cb96eb6f6db99000a44
Touchscreen PC <sageroboticsbyhalia@gmail.com>
2025-10-15 23:26:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// --- Firebase imports ---
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+// --- Firebase config (from your Firebase Console) ---
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDXS6Lftgfl9-rzA0ljIj0YCSpF5_fp4FI",
+    authDomain: "almighty-test.firebaseapp.com",
+    databaseURL: "https://almighty-test-default-rtdb.firebaseio.com",
+    projectId: "almighty-test",
+    storageBucket: "almighty-test.firebasestorage.app",
+    messagingSenderId: "942284018380",
+    appId: "1:942284018380:web:5a97cd851a9e03ff536869",
+    measurementId: "G-V0E4WCMN9Q"
+  };
+
+// --- Initialize ---
+const app = initializeApp(firebaseConfig);
+console.log("🔍 Connected to Firestore project:", firebaseConfig.projectId);
+
+const db = getFirestore(app);
+
+
+async function addMembers() {
+  try {
+    const docRef = await addDoc(collection(db, "Members"), {
+      name: "Stepahanie's Museum",
+      category: "Historical",
+      location: "Southaven, MS",
+      rating: 4.8,
+      Like: "No",
+      description: "A small-town museum highlighting local heritage.",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    console.log("✅ Document written with ID:", docRef.id);
+  } catch (e) {
+    console.error("❌ Error adding document:", e);
+  }
+}
+
+addMembers();
+
